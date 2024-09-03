95869c184e6cf97be833b186561837ea98cfce34
Alejandro Cadavid <alecad76.ac@gmail.com>
2024-09-03 12:10:55 -0500

    Add DB config file and test

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import pool from "./database/db_connect";
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req, res)=>{
+app.get('/', async (req, res)=>{
+    const query = 'select * from informacion_nino;';
+    const response = await pool.query(query);
+    console.log(response);
     res.send('Hola Mundo, el Servidor está en el puerto 3000. --- Alejandro Cadavid')
 });
 
