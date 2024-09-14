829a17bd4b8bf73ae07e9b12d01df593aaa63dc9
Alejandro Cadavid <alecad76.ac@gmail.com>
2024-09-14 18:59:03 -0500

    Solo se tocó para pruebas, no se modificó nada, pero YA ESTÁN FUNCIONANDO LOS DOS FORMULARIOS. LOGIN Y REGISTER

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const port = process.env.PORT;
 const objectNinoRoute = Router();
 const userRoutes = Router();
 
-objectNinoRoute.post('/create_informacion_nino', /*authenticateToken,*/ createNino);
+objectNinoRoute.post('/create_informacion_nino', authenticateToken, createNino);
 objectNinoRoute.get('/get_informacion_nino', authenticateToken, getNino);
 objectNinoRoute.delete('/delete_informacion_nino/:id', authenticateToken, deleteNino);
 objectNinoRoute.put('/update_informacion_nino/:id', authenticateToken, updateNino);
@@ -30,10 +30,3 @@ app.listen(port, ()=>{
     return console.log(`Example app listening on port ${port}`)
 });
 
-
-/*app.get('/', async (req, res)=>{
-    const query = 'select * from informacion_nino;';
-    const response = await pool.query(query);
-    console.log(response);
-    res.send('Hola Mundo, el Servidor está en el puerto 3000. --- Alejandro Cadavid')
-});*/
--- src/middleware/authorization_ToknMiddl.ts
+++ src/middleware/authorization_ToknMiddl.ts
@@ -4,13 +4,13 @@ import jwt from "jsonwebtoken";
 require('dotenv').config();
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-    const authHeaders = req.headers ['authorization'];
+    const authHeaders = req.headers ["authorization"];
+
     const token = authHeaders && authHeaders.split(' ')[1];
     if(!token){
         return res.status(401).json({error: 'Auth Token Not Found'});
     }
     jwt.verify(token, `${process.env.CLAVE_JWT}`, (err, user) =>{
-        console.log(user);
         if(err){
             return res.status(403).json({error: 'Invalid Token'})
         }
